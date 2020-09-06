3a3d4357a6bfa7fcb6bc4cb7a1c7d1d1cabba7d1
Narit Charoenval <naritsociety@gmail.com>
2020-09-06 16:26:28 +0700

    Add go back button

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -3,11 +3,23 @@ import { Text, StyleSheet, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-const DetailScreen = () => {
+const DetailScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>FarmDetail</Text>
+      <Text style={styles.bigText}>FarmDetail</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push("Details")}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  bigText: {
+    fontSize: 30,
+  },
+});
 export default DetailScreen;
